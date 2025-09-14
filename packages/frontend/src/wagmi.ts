7d4a02477d0811e486c8b9a0eb8db6018fda17db
communitymanager <communitymanager@engineercafe.jp>
2025-09-14 07:04:24 +0000

    add counter sample

--- packages/frontend/src/wagmi.ts
+++ packages/frontend/src/wagmi.ts
@@ -1,5 +1,7 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { defineChain } from 'viem';
 import {
+  anvil,
   arbitrum,
   base,
   mainnet,
@@ -17,6 +19,22 @@ export const config = getDefaultConfig({
     optimism,
     arbitrum,
     base,
+    anvil,
+    defineChain({
+      id: 31_337,
+      name: 'Local',
+      nativeCurrency: {
+        decimals: 18,
+        name: 'Ether',
+        symbol: 'ETH',
+      },
+      rpcUrls: {
+        default: {
+          http: ['http://127.0.0.1:8545'],
+          webSocket: ['ws://127.0.0.1:8545'],
+        },
+      },
+}),
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
   ],
   ssr: true,
